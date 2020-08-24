aeba88fcdffd239e934a2e83a702cc3c2556808d
joseignasus <joseignasus@gmail.com>
2020-08-24 12:53:29 +0200

    Login with google and sign in with google

--- app/src/main/java/com/example/city_o/Login.kt
+++ app/src/main/java/com/example/city_o/Login.kt
@@ -8,7 +8,15 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_register.*
 import kotlin.properties.Delegates
 
 class Login : AppCompatActivity() {
@@ -23,6 +31,9 @@ class Login : AppCompatActivity() {
 
     //This variable is for firebase authentication
     private lateinit var mAuth: FirebaseAuth
+    val RC_SIGN_IN : Int = 1
+    lateinit var mGoogleSignInClient : GoogleSignInClient
+    lateinit var mGoogleSignInOptions : GoogleSignInOptions
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +47,7 @@ class Login : AppCompatActivity() {
         etPassword = findViewById(R.id.etPassword)
         mProgressBar = ProgressBar(this)
         mAuth = FirebaseAuth.getInstance()
+        configureGoogleSignIn()
     }
 
     private fun loginUser() {
@@ -59,6 +71,58 @@ class Login : AppCompatActivity() {
         }
     }
 
+    //****** SING UP WITH GOOGLE ACCOUNT *******
+
+    private fun configureGoogleSignIn() {
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions)
+    }
+
+
+    /*private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }*/
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                updateUserInfoAndGoHome()
+            } else {
+                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun updateUserInfoAndGoHome() {
+        //Nos vamos a la actividad home
+        val intent = Intent(this, Login::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    /************** END OF SING UP ***********/
+
+    /************** BUTTONS EVENTS******************/
     private fun goHome() {
         //mProgressBar.hide()
         val intent = Intent(this, MainActivity::class.java)
@@ -70,14 +134,13 @@ class Login : AppCompatActivity() {
         loginUser()
     }
 
-   /** fun forgotPassword(view: View) {
-        startActivity(Intent(this,
-            ForgotPasswordActivity::class.java))
-    }*/
-    fun register(view: View) {
-        startActivity(Intent(this, RegisterActivity::class.java))
+    fun setupUI(view: View) {
+        google_button.setOnClickListener {
+            val signInIntent: Intent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
     }
 
-
+    /************** END OF BUTTONS EVENTS******************/
 
 }
